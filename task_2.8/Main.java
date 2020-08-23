226ffbacaea32d659c2b72282afcdcfbdd2eca3f
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-24 00:11:48 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+        int a;
+        int b;
+        for(a = 1 ; a <= 9; a++){
+            System.out.println("8");
+            for(b=1 ; b <=a;b++)
+                System.out.print("8");
+
+        }
+        System.out.println(8);
