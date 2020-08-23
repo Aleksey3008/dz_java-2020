a8e9bfe47c631b77772e550aa2d06920af17fcea
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-23 22:07:16 +0300

    Create Main.java

---
+++ task_2.6/Main.java
@@ -0,0 +1,12 @@
+int i=1,n=1;
+        while(i<=10)
+        {
+            System.out.print(i+" ");
+            while(n<=9)
+            {
+                n++;
+                System.out.print(i*n+" ");
+            }
+            i++;
+            n=1;
+            System.out.println("");
