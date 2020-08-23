95f43b6e32ff662b97bcd049c947f7028f320d7f
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-23 21:09:32 +0300

    Delete Main.java

--- task_1.7/Main.java
+++
@@ -1,9 +0,0 @@
-public static void main(String[] args) {
-        System.out.println(getVolume(25, 5, 2));
-    }
-
-    public static long getVolume(int a, int b, int c) {
-        a = 250;
-        b = 50;
-        c = 20;
-    return a * b * c;
