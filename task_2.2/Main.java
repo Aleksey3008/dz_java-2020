4712e41984f42b151777a1a6ba50c0597d991d2c
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-24 17:10:12 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -20,8 +20,10 @@ public static int min(int a, int b, int c, int d) {
     }
 
     public static void main(String[] args) throws Exception {
-            System.out.println(min(-20, -10));
-            System.out.println(min(-40, -10, -30, 40));
-            System.out.println(min(-20, -40, -30, 40));
-            System.out.println(min(-20, -10, -40, 40));
-            System.out.println(min(-20, -10, -30, -40));
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
+}
