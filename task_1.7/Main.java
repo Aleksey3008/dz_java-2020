fc9fed3e92e47a19e2307d5b747242f67e4e7fb4
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-24 17:03:59 +0300

    Update Main.java

--- task_1.7/Main.java
+++ task_1.7/Main.java
@@ -1,7 +1,12 @@
-System.out.println(getVolume(25, 5, 2));
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getVolume(25, 5, 2));
     }
 
 
     public static long getVolume(int a, int b, int c) {
         long d = a*b*c*1000;
         return d;
+    }
+}
