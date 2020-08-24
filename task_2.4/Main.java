4b11549cde420b5c675312e9ff123d6506f38cda
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-24 17:24:11 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -1,6 +1,11 @@
-public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
         int a=0;
         for (int i = 1; i <= 5; i++){
 
             a = a + i;
             System.out.println(a);
+
+        }
+    }
+}
