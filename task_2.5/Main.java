6b77c04185c1cf13cfda32d2006670eb9f97a4c0
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-24 17:34:44 +0300

    Update Main.java

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -1,38 +1,15 @@
-public static void main(String[] args) {
-        String cleaning [] = {"Мама", "Мыла", "Раму"};
-        perm(cleaning, 0);
+public class Main {
+    public static void main(String[] args)
+    {
+        String a = "Мама";
+        String b= "Мыла";
+        String c= "Раму";
 
-
-    }
-    public static void perm(String[] arr, int k){
-        if (k==arr.length) {
-            printstr(arr);
-        }
-        else {
-            int grand[] = new int [arr.length-k];
-            for (int i=k, n=0; i<arr.length; i++,n++) {
-                grand[n]=i;
-            }
-            for (int i=0; i<grand.length; i++) {
-                String z;
-                z=arr[k];
-                arr[k]=arr[grand[i]];
-                arr[grand[i]]=z;
-                perm(arr, k+1);
-                z=arr[k];
-                arr[k]=arr[grand[i]];
-                arr[grand[i]]=z;
-            }
-
-        }
-    }
-    public static void printstr(String[] arr){
-
-        String teString="";
-        for (int i=0; i<arr.length; i++) {
-            teString = teString + arr[i];
-        }
-        System.out.println(teString);
-
+        System.out.println(a+b+c);
+        System.out.println(a+c+b);
+        System.out.println(b+a+c);
+        System.out.println(b+c+a);
+        System.out.println(c+a+b);
+        System.out.println(c+b+a);
     }
 }
