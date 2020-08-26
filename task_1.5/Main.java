84bf2289c6435e5e2d2cafbddec2931517256d5a
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-26 18:39:20 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -1,9 +1,11 @@
-public static void main(String[] args){
+public class Main {
+    public static void main(String[] args){
         System.out.println(convertCelsiumToFahrenheit(41));
     }
 
     public static double convertCelsiumToFahrenheit(int celsium) {
-        double fahrenheit = 9 / 5.0 * celsium + 32;
+        double fahrenheit = (celsium *(9/5.0)) + 32;
         return fahrenheit;
+
     }
 }
